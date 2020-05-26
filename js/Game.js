469319521f7120d161da57c96f2532bf0784ee95
Cam <cameron.riemensnider@gmail.com>
2020-05-26 09:14:52 -0700

    phrase and game classes initialized

--- js/Game.js
+++ js/Game.js
@@ -2,8 +2,10 @@
  * Project 4 - OOP Game App
  * Game.js */
 
- class Game() {
-    this.missed = 0;
-    this.phrases = [];
-    this.activePhrase = null;
+ class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [];
+        this.activePhrase = null;
+    }
  }
