ed32ceaa1e4cdad6d502a2fcc2bb922b3cb03a50
Cam <cameron.riemensnider@gmail.com>
2020-05-27 09:26:29 -0700

    test code

--- js/Phrase.js
+++ js/Phrase.js
@@ -11,58 +11,58 @@
 * Display phrase on game board
 */
 
-    addPhraseToDisplay() {
-       const phraseUl = document.querySelector('#phrase ul');
-       for (let i = 0; i < this.phrase.length; i++) {
-           if (this.phrase.charAt(i) === ' ') {
-               const phraseSpaceLi = document.createElement('li');
-               phraseSpaceLi.textContent = ' ';
-               phraseSpaceLi.setAttribute('class', 'space');
-               phraseUl.appendChild(phraseSpaceLi);
-           } else {
-               const phraseLetterLi = document.createElement('li');
-               phraseLetterLi.textContent = this.phrase[i];
-               phraseLetterLi.setAttribute('class', `hide letter ${this.phrase[i]}`);
-               phraseUl.appendChild(phraseLetterLi);
-           }
-       }
-    }
+//     addPhraseToDisplay() {
+//        const phraseUl = document.querySelector('#phrase ul');
+//        for (let i = 0; i < this.phrase.length; i++) {
+//            if (this.phrase.charAt(i) === ' ') {
+//                const phraseSpaceLi = document.createElement('li');
+//                phraseSpaceLi.textContent = ' ';
+//                phraseSpaceLi.setAttribute('class', 'space');
+//                phraseUl.appendChild(phraseSpaceLi);
+//            } else {
+//                const phraseLetterLi = document.createElement('li');
+//                phraseLetterLi.textContent = this.phrase[i];
+//                phraseLetterLi.setAttribute('class', `hide letter ${this.phrase[i]}`);
+//                phraseUl.appendChild(phraseLetterLi);
+//            }
+//        }
+//     }
 
-/**
-* Checks if passed letter is in phrase
-* @param (string) letter - Letter to check
-*/
+// /**
+// * Checks if passed letter is in phrase
+// * @param (string) letter - Letter to check
+// */
 
-    checkLetter(letter) {
-        const correctLetters = [];
-        for (let i = 0; i < this.phrase.length; i++) {
-            if (letter = this.phrase[i].textContent) {
-                correctLetters.push(letter);
-            }
-        }
-        if (correctLetters.length !== 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+//     checkLetter(letter) {
+//         const correctLetters = [];
+//         for (let i = 0; i < this.phrase.length; i++) {
+//             if (letter = this.phrase[i].textContent) {
+//                 correctLetters.push(letter);
+//             }
+//         }
+//         if (correctLetters.length !== 0) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
 
-/**
-* Displays passed letter on screen after a match is found
-* @param (string) letter - Letter to display
-*/
+// /**
+// * Displays passed letter on screen after a match is found
+// * @param (string) letter - Letter to display
+// */
 
-    showMatchedLetter(letter) {
-        for (let i = 0; i < this.phrase.length; i++) {
-            if (letter = this.phrase[i]) {
-                const matchedLetters = document.querySelectorAll(`.${letter}`);
-                for (let i = 0; i < matchedLetters.length; i++) {
-                    matchedLetters[i].classList.remove('hide');
-                    matchedLetters[i].classList.add('show');
-                }
-            }
-        }
-    }
+//     showMatchedLetter(letter) {
+//         for (let i = 0; i < this.phrase.length; i++) {
+//             if (letter = this.phrase[i]) {
+//                 const matchedLetters = document.querySelectorAll(`.${letter}`);
+//                 for (let i = 0; i < matchedLetters.length; i++) {
+//                     matchedLetters[i].classList.remove('hide');
+//                     matchedLetters[i].classList.add('show');
+//                 }
+//             }
+//         }
+//     }
 
 
  }
--- js/app.js
+++ js/app.js
@@ -7,8 +7,12 @@
  * button, the game will begin by calling startGame(). 
  */
 
-let game;
-document.getElementById('btn__reset').addEventListener('click', (e) =>{
-    game = new Game();
-    game.startGame();
+// let game;
+// document.getElementById('btn__reset').addEventListener('click', (e) =>{
+//     game = new Game();
+//     game.startGame();
+// });
+const game = new Game();
+game.phrases.forEach((phrase, index) => {
+console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
 });
