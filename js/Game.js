09d9e8421d07fe7a42033dd68623528ebf6a4814
Cam <cameron.riemensnider@gmail.com>
2021-03-03 16:14:55 -0800

    Changing format

--- js/Game.js
+++ js/Game.js
@@ -110,13 +110,13 @@
         document.getElementById('overlay').style.visibility = 'visible';
         if (gameWon) {
             document.getElementById('overlay').className = 'win';
-            document.getElementById('game-over-message').textContent = `Congratulations! You guessed the phrase! The phrase was ${this.activePhrase.phrase}!`;
+            document.getElementById('game-over-message').textContent = `Congratulations! You guessed the phrase! The phrase was "${this.activePhrase.phrase}!"`;
             gamesWon++;
             gamesPlayed++;
             winPercentage = ((gamesWon / gamesPlayed) * 100).toFixed(2).toString() + '%';
         } else {
             document.getElementById('overlay').className = 'lose';
-            document.getElementById('game-over-message').textContent = `Boo! You didn't get the phrase this time! The phrase was ${this.activePhrase.phrase}!`
+            document.getElementById('game-over-message').textContent = `Boo! You didn't get the phrase this time! The phrase was "${this.activePhrase.phrase}!"`
             gamesPlayed++;
             winPercentage = ((gamesWon / gamesPlayed) * 100).toFixed(2).toString() + '%';
         }
